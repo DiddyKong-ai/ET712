00cc9fbad86dfbf67c8301de27fe0db87721031c
DiddyKong-ai <dillonrobertson969@gmail.com>
2025-02-13 15:37:59 -0500

    Update lab_4robertson.js

--- lab4/lab_4robertson.js
+++ lab4/lab_4robertson.js
@@ -60,3 +60,36 @@ function checktemp(temperature){
 let t = 100
 let temp_result = checktemp(t)
 console.log(`Is the temprature greater than 75? ${temp_result}`)
+// bulit-in function in JS is called as a global variable
+// Math object
+let num_pi = Math.PI
+console.log(`JS PI value = ${num_pi}`)
+// round() method returns the nearest integer
+console.log(`rounded up PI = ${Math.round(num_pi)}`)
+// ceil() method returns the round up of a number
+console.log(`ceil method applied to PI = ${Math.ceil(num_pi)}`)
+// pow() method returns the value of base to the power of exponent
+console.log(`2^3 = ${Math.pow(2,3)}`)
+// square root method --> sqrt()
+console.log(`square root of 9 = ${Math.sqrt(20)}`)
+// random number generator, random() method returns a random number between 0 and 1
+console.log(`Random number = ${Math.random()}`)
+// random number between 1 and 9 
+let n = 1 + Math.round(Math.random()*8)
+console.log(`Number between 1 and 9 = ${n}`)
+// function to randomly generate a number between -5 and 5
+function randnumber() {
+    let p = -5 + Math.round(Math.random() * 10)
+    return p;
+}
+
+// function to randomly pick a color from a list of colors
+let colors = ["green", "blue", "red", "orange", "pink"]
+function randomcolor(picked_index){
+    let list_size = colors.length
+    let last_index = list_size - 1
+    let random_index = Math.round(Math.random()*last_index) // randomly pick a number between 0 and the last index
+    return colors[random_index]     // return the random picked color
+}
+
+console.log(`Color picked = ${randomcolor()}`)
