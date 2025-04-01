891af5c1c809ef8266b58038c6b0d13ad7d98333
DiddyKong-ai <dillonrobertson969@gmail.com>
2025-04-01 19:36:40 +0000

    update lab 11

---
+++ lab11/first_app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Dillon Robertson</h1>
+      <p>This is my first ReactJS app</p>
+    </div>
+  );
+}
+
+export default App;
