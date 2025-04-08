b3f97ec7576ef5619c74b1f190411ffa229bb935
DiddyKong-ai <dillonrobertson969@gmail.com>
2025-04-08 20:01:26 +0000

    complete props

---
+++ lab12/props_react/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import User from "./comments"
+import "./index.css"
+// import images
+import papermario from "./images/paper_mario.png"
+import boo from "./images/boo.png"
+import luma from "./images/luma.png"
+
+function App() {
+  return (
+    <>
+      <h1>User Comments</h1>
+      <main className='maincontainer'>
+        <User/>
+      <User/>
+      <User/>
+      <User/>  
+      </main>
+      
+    
+    </>
+  );
+}
+
+export default App;
