43b55872e618bf17b3077739ef2e3a0c83797808
DiddyKong-ai <dillonrobertson969@gmail.com>
2025-04-10 19:56:09 +0000

    complete up to close modal window

--- lab12/props_react/src/App.js
+++ lab12/props_react/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import User from "./comments"
 import "./index.css"
+import User_feedback from './feedback'
+import Modalwindow from "./modalwindow"
 // import images
 import papermario from "./images/paper_mario.png"
 import boo from "./images/boo.png"
@@ -11,12 +13,12 @@ function App() {
     <>
       <h1>User Comments</h1>
       <main className='maincontainer'>
-        <User/>
-      <User/>
-      <User/>
-      <User/>  
+        <User_feedback/><User image={papermario} username="Mario" date="04/10/25" usercomment="Good Job!"/><User_feedback/>
+        <User_feedback/><User image={boo} username="Boo" date="04/5/25" usercomment="Super!"/><User_feedback/>
+        <User_feedback/><User image={luma} username="Luma" date="04/24/25" usercomment="Excellent!"/><User_feedback/>
       </main>
-      
+
+    
     
     </>
   );
